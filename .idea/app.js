71e4af5fcf4c8e72366567222240c0aef3ddf4e2
Alexey Uporoyskiy <auporoyskiy@aquivalabs.com>
2022-02-21 12:18:09 +0100

    Initial frame

--- .idea/app.js
+++ .idea/app.js
@@ -11,14 +11,14 @@ const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
-//const LocalStrategy = require('passport-local');
-//const User = require('./models/user');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-//const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/users');
 //const campgroundRoutes = require('./routes/campgrounds');
 //const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect('mongodb://localhost:27017/explore-it', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
-/*
+
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
     resave: false,
@@ -52,8 +52,8 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-//app.use(session(sessionConfig))
-//app.use(flash());
+app.use(session(sessionConfig))
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -67,6 +67,7 @@ app.use((req, res, next) => {
     next();
 })
 app.use('/', userRoutes);
+/*
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/reviews', reviewRoutes)
 */
