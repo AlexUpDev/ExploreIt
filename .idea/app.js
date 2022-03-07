93d2d2e62a1abb5034c59c4bad160ad0590cebf4
Alexey Uporoyskiy <auporoyskiy@aquivalabs.com>
2022-03-07 18:28:29 +0100

    Labels adding

--- .idea/app.js
+++ .idea/app.js
@@ -84,7 +84,7 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err })
+    res.status(statusCode).render('error', { constants, err })
 })
 
 app.listen(3000, () => {
--- .idea/public/javascripts/navbar.js
+++ .idea/public/javascripts/navbar.js
@@ -1,3 +1,12 @@
+document.getElementById('eng-select').addEventListener('click', function() {
+    console.log("Eng!");
+
+});
+
+document.getElementById('rus-select').addEventListener('click', function() {
+    console.log("Rus!");
+});
+
 document.getElementById('login-link').addEventListener('click', function() {
     console.log("hello");
 });
